a6208cf14d5ba29ee12eef4284a361ce979e7705
FiXtal4 <adamadamob1202@gmail.com>
2025-09-29 16:41:00 +0300

    sucky commit

--- cr_dir.c
+++ cr_dir.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
 #include <sys/stat.h>
 #include <dirent.h>
 #include "cr_dir.h"
@@ -7,23 +8,20 @@ void create_directories(){
     if(mkdir("Assets", 0755) == 0){
         puts("Directory Assets create DONE...");
     }
-    else{
+    else if (errno == EEXIST){
         puts("WARNING You have directory Assets");
-        return;
     }
     if(mkdir("Assets/Scripts", 0755) == 0){
         puts("Directory Assets/Scripts create DONE...");
     }
-    else if(mkdir("Assets/Scripts", 0755) == -1){
+    else if(errno == EEXIST){
         puts("WARNING You have directory Assets/Scripts");
-        return;
     }
     if(mkdir("Assets/Textures", 0755) == 0){
         puts("Directory Assets/Textures create DONE...");
     }
-    else{
+    else if(errno == EEXIST){
         puts("WARNING You have directory Assets/Textures");
-        return;
     }
 }
 
@@ -33,9 +31,5 @@ void check_directories(const char *path){
         fprintf(stderr, "Open %s directory ERROR\n", path);
         return;
     }
-    struct dirent *en_path;
-    while((en_path = readdir(op_path)) != NULL){
-        fprintf(stdout, "In directory %s have %s\n", path, en_path->d_name);
-    }
     closedir(op_path);
-}
+} 
---
+++ main.c
@@ -0,0 +1,101 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include "cr_dir.h"
+int main(){
+    const char *dir_assets = "Assets";
+    const char *dir_textures = "Assets/Textures";
+    const char *dir_scripts = "Assets/Scripts";
+
+    struct stat dir_info;
+
+    create_directories();
+    check_directories(dir_assets);
+    check_directories(dir_textures);
+    check_directories(dir_scripts);
+
+    if(mkdir("dir", 0755) == 0){
+        fprintf(stdout, "Create directory dir DONE...\n");
+    }
+    else{
+        printf("Directory has already been created\n");
+    }
+
+    if(stat("dir", &dir_info) == 0){
+        fprintf(stdout, "find \033[32mdirectory dir\033[0m\n");        
+    }
+    else if(stat("dir", &dir_info) == -1){
+        perror("stat failed");
+        return EXIT_FAILURE;
+    }
+
+    DIR *op_dir = opendir("dir");
+
+    if(op_dir == NULL){
+        perror("Open directory failed");
+        return EXIT_FAILURE;
+    }
+    else{
+        fprintf(stdout, "Open directory DONE...\n");
+    }
+
+    struct dirent *en_dir;
+
+    while((en_dir = readdir(op_dir)) != NULL){
+       if(strcmp(en_dir->d_name, ".") == 0 || strcmp(en_dir->d_name, "..") == 0){
+           continue;
+       }
+       fprintf(stdout, "In directory dir have %s\n", en_dir->d_name);
+       if(en_dir->d_type == DT_DIR){
+           fprintf(stdout, "Find and read directory dir\n");
+           break;
+        }
+    }
+
+    FILE *op_file = fopen("dir/hello", "w");
+
+    if(op_file == NULL){
+        perror("fopen file failed");
+        return EXIT_FAILURE;
+    }
+    else{
+        fprintf(stdout, "Open file path dir/hello \033[32mDONE...\033[0m\n");
+    }
+
+    char *text = "#!/bin/bash\necho \"Hello, World!\"\n";
+    int write_text = fprintf(op_file, "%s", text);
+    if(write_text < 0){
+        perror("Write text failed");
+        return EXIT_FAILURE;
+    }
+    else{
+        printf("Write text DONE...\n");
+    }
+    fclose(op_file);
+
+    FILE *r_file = fopen("dir/hello", "rb");
+
+    if(r_file == NULL){
+        perror("fopen file failed");
+        return EXIT_FAILURE;
+    }
+    fclose(r_file);
+    int ch_per = chmod("dir/hello", 0755);
+    if(ch_per == -1){
+        perror("chmod failed");
+        return EXIT_FAILURE;
+    }
+    else{
+        fprintf(stdout, "Change file hello execution permissions \033[32mDONE...\033[0m\n");
+    }
+    fprintf(stdout, "File execution...\n");
+    fflush(stdout);
+    closedir(op_dir);
+    execl("dir/hello", "dir/hello", NULL);
+    return 0;
+}
+
